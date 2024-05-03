5a99c4c8a3eaab4d852f9d935a0a26c79974001f
Samozvanec1 <usackikhs@yandex.ru>
2024-05-03 23:12:19 +0300

    add-new-const

--- GlavFail.py
+++ GlavFail.py
@@ -1,8 +1,13 @@
 import pygame
 pygame.init()
 
+SCREEN_WIDTH = 880
+SCREEN_HEIGHT = 660
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+pygame.display.set_caption("Игра В ТИР")
 
+icon = pygame.image.load("")
 running = True
 while  running:
     pass
