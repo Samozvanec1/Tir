aa841733be08c29deebdcc26709289c4e86159c8
Samozvanec1 <usackikhs@yandex.ru>
2024-05-03 23:46:54 +0300

    add all const

--- GlavFail.py
+++ GlavFail.py
@@ -1,3 +1,5 @@
+import random
+
 import pygame
 pygame.init()
 
@@ -7,7 +9,18 @@ screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
 pygame.display.set_caption("Игра В ТИР")
 
-icon = pygame.image.load("")
+icon = pygame.image.load("img/Стрелы")
+pygame.display.set_icon(icon)
+
+target_img = pygame.image.load("img/target.png")
+target_width = 50
+target_height = 50
+
+target_x = random.randint( 1, SCREEN_WIDTH - (target_width + 1))
+target_y = random.randint( 1, SCREEN_HEIGHT - (target_height + 1))
+
+color = (random.randint (0, 255), random.randint (0, 255), random.randint(0, 255))
+
 running = True
 while  running:
     pass
