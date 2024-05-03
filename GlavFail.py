bcdf7e079910e2f7e71f98709a9904845e9d6bf6
Samozvanec1 <usackikhs@yandex.ru>
2024-05-04 00:23:28 +0300

    today finisch

--- GlavFail.py
+++ GlavFail.py
@@ -9,7 +9,7 @@ screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
 pygame.display.set_caption("Игра В ТИР")
 
-icon = pygame.image.load("img/Стрелы")
+icon = pygame.image.load("img/Стрелы.jpg")
 pygame.display.set_icon(icon)
 
 target_img = pygame.image.load("img/target.png")
@@ -30,8 +30,8 @@ while  running:
         if event.type == pygame.MOUSEBUTTONDOWN:
             mouse_x, mouse_y = pygame.mouse.get_pos()
             if target_x < mouse_x < target_x + target_width and target_y < mouse_y < target_y + target_height:
-                target_x = random.randint(1, SCREEN_WIDTH - (target_width + 1))
-                target_y = random.randint(1, SCREEN_HEIGHT - (target_height + 1))
+                target_x = random.randint(0, SCREEN_WIDTH - (target_width))
+                target_y = random.randint(0, SCREEN_HEIGHT - (target_height))
     screen.blit(target_img, (target_x, target_y))
 
     pygame.display.update()
