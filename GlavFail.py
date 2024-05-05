55accdf71a98988c6538ffe86e6317be6bed4e09
WersKaos <110294243+WersKaos@users.noreply.github.com>
2024-05-05 23:53:36 +0300

    MathTryingToDeleteAllErrors
    i cant :cry:

--- GlavFail.py
+++ GlavFail.py
@@ -1,12 +1,72 @@
 import random
-
+import heapq
 import pygame
 pygame.init()
+def manhattan_distance(point1, point2):
+    return abs(point1[0] - point2[0]) + abs(point1[1] - point2[1])
+def astar(grid, start, goal):
+    # Инициализация словарей для хранения стоимостей и предыдущих точек
+    cost = {start: 0}
+    came_from = {start: None}
+
+
+    # Приоритетная очередь для хранения точек и их оценок (приоритетов)
+    priority_queue = [(0, start)]
+
+
+    while priority_queue:
+
+        current_cost, current_point = heapq.heappop(priority_queue)
+
+        # Если достигнута целевая точка, завершаем алгоритм
+        if current_point == goal:
+            break
+
+
+    for neighbor in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
+        x, y = current_point[0] + neighbor[0], current_point[1] + neighbor[1]
+
+
+    new_cost = cost[current_point] + 1 # Стоимость перемещения на одну ячейку
+
+
+
+
+    if 0 <= x < len(grid) and 0 <= y < len(grid[0]) and grid[x][y] == 0:
+        if (x, y) not in cost or new_cost < cost[(x, y)]:
+            cost[(x, y)] = new_cost
+            priority = new_cost + manhattan_distance((x, y), goal)  
+                    # Оценка для приоритета
+            heapq.heappush(priority_queue, (priority, (x, y)))
+            came_from[(x, y)] = current_point
+            path = []
+    current = goal
+    while current:
+        path.append(current)
+        current = came_from[current]
+    path.reverse()
+
+
+    return path
+def create_grid(SCREEN_HEIGHT, SCREEN_WIDTH):
+    main_list = []
+    append_list = []
+    for i in range(SCREEN_HEIGHT):
+        for i in range(SCREEN_WIDTH):
+            append_list.append(0)
+        main_list.append(append_list)
+        append_list = []
+    return main_list
+        
+
 
 SCREEN_WIDTH = 880
 SCREEN_HEIGHT = 660
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-
+print(create_grid(SCREEN_HEIGHT, SCREEN_WIDTH))
+grid = create_grid(SCREEN_HEIGHT, SCREEN_WIDTH)
+for point in astar(grid, (30,50), (43,97)):
+    print(point)
 pygame.display.set_caption("Игра В ТИР")
 
 icon = pygame.image.load("img/Стрелы.jpg")
