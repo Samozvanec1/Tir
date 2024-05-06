83350a6aefbbf4b7749344722eacdf1fc592b9a1
Samozvanec1 <usackikhs@yandex.ru>
2024-05-06 21:41:39 +0300

    iv11

--- GlavFail.py
+++ GlavFail.py
@@ -4,61 +4,48 @@ import pygame
 pygame.init()
 def manhattan_distance(point1, point2):
     return abs(point1[0] - point2[0]) + abs(point1[1] - point2[1])
+
 def astar(grid, start, goal):
-    # Инициализация словарей для хранения стоимостей и предыдущих точек
     cost = {start: 0}
     came_from = {start: None}
-
-
-    # Приоритетная очередь для хранения точек и их оценок (приоритетов)
     priority_queue = [(0, start)]
 
-
     while priority_queue:
-
         current_cost, current_point = heapq.heappop(priority_queue)
 
-        # Если достигнута целевая точка, завершаем алгоритм
         if current_point == goal:
             break
 
+        # Проверка соседних точек
+        for neighbor in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
+            x, y = current_point[0] + neighbor[0], current_point[1] + neighbor[1]
 
-    for neighbor in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
-        x, y = current_point[0] + neighbor[0], current_point[1] + neighbor[1]
+            if 0 <= x < len(grid) and 0 <= y < len(grid[0]) and grid[x][y] == 0:
+                new_cost = cost[current_point] + 1
 
+                if (x, y) not in cost or new_cost < cost[(x, y)]:
+                    cost[(x, y)] = new_cost
+                    priority = new_cost + manhattan_distance((x, y), goal)
+                    heapq.heappush(priority_queue, (priority, (x, y)))
+                    came_from[(x, y)] = current_point
 
-    new_cost = cost[current_point] + 1 # Стоимость перемещения на одну ячейку
-
-
-
-
-    if 0 <= x < len(grid) and 0 <= y < len(grid[0]) and grid[x][y] == 0:
-        if (x, y) not in cost or new_cost < cost[(x, y)]:
-            cost[(x, y)] = new_cost
-            priority = new_cost + manhattan_distance((x, y), goal)  
-                    # Оценка для приоритета
-            heapq.heappush(priority_queue, (priority, (x, y)))
-            came_from[(x, y)] = current_point
-            path = []
+    # Построение пути
+    path = []
     current = goal
     while current:
         path.append(current)
         current = came_from[current]
     path.reverse()
 
-
     return path
 def create_grid(SCREEN_HEIGHT, SCREEN_WIDTH):
-    main_list = []
-    append_list = []
-    for i in range(SCREEN_HEIGHT):
-        for i in range(SCREEN_WIDTH):
-            append_list.append(0)
-        main_list.append(append_list)
-        append_list = []
-    return main_list
-        
-
+     main_list = []
+     for i in range(SCREEN_HEIGHT):
+         append_list = []
+         for j in range(SCREEN_WIDTH):
+            append_list.append(random.choice([0, 1]))
+            main_list.append(append_list)
+     return main_list
 
 SCREEN_WIDTH = 880
 SCREEN_HEIGHT = 660
